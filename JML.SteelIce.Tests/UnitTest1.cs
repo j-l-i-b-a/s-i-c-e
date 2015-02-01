6fab965b80fdc0c99be4ee8bfaa471d89dae7856
Juraj Liba <jurajliba@gmail.com>
2015-02-01 18:26:12 +0100

    Gitignore, unitest

---
+++ JML.SteelIce.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JML.SteelIce.Tests
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void SayHello_Test()
+        {
+            Console.WriteLine("Hello you");
+        }
+    }
+}
